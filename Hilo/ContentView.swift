9b4f51dcb6f74e1e294ae933e3273c0c11cab3ae
Yhon Vivas <cactu@Mac-mini-de-Yhon.local>
2026-01-31 16:09:50 -0300

    first commit

---
+++ Hilo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Hilo
+//
+//  Created by Yhon Vivas on 31-01-26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hilo/HiloApp.swift
@@ -0,0 +1,17 @@
+//
+//  HiloApp.swift
+//  Hilo
+//
+//  Created by Yhon Vivas on 31-01-26.
+//
+
+import SwiftUI
+
+@main
+struct HiloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
